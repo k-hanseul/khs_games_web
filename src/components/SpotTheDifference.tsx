97bc6b633c6b6530ee0c5d722b0caf745b50bc5f
KimHanseul <lkj2822@gmail.com>
2025-04-14 22:34:51 +0900

    20250414-khs-1

--- src/components/SpotTheDifference.tsx
+++ src/components/SpotTheDifference.tsx
@@ -3,19 +3,57 @@ import { useEffect, useState, useRef } from "react";
 <area target="" alt="" title="" href="" coords="314,548,57" shape="circle">
 <area target="" alt="" title="" href="" coords="565,543,40" shape="circle"> */}
 const SpotTheDifference = () => {
-    // 이미지 정답 좌표
-    const imgPoint: number[][][] = [
+
+    // const handleClick = useCallback(() => {
+    //     doSomething();
+    // }, [dependency]);
+
+    // 스테이지 이미지
+    const stageImg: string[][] = [
         [
-            [414, 222, 43], [314, 548, 57], [565, 543, 40]
+            process.env.PUBLIC_URL + '/img/SpotTheDifference/stage_1_1.png',
+            process.env.PUBLIC_URL + '/img/SpotTheDifference/stage_1_2.png'
+        ],
+        [
+            process.env.PUBLIC_URL + '/img/SpotTheDifference/stage_1_1.png',
+            process.env.PUBLIC_URL + '/img/SpotTheDifference/stage_1_2.png'
+        ]
+    ];
+
+    // 이미지 정답 좌표
+    interface point { x: number, y: number, s: number };
+    const stagePoint: point[][] = [
+        [
+            { x: 414, y: 222, s: 45 },
+            { x: 314, y: 548, s: 45 },
+            { x: 565, y: 543, s: 45 }
+        ],
+        [
+            { x: 4, y: 4, s: 4 },
+            { x: 5, y: 5, s: 5 },
+            { x: 6, y: 6, s: 6 }
         ]
     ];
 
     // 스테이지, 라이프, 타이머, 힌트, 갯수
-    const stage: number = 1;
+    const [stage, setStage] = useState(0);
+
+    // let stage: number = 1;
     const life: number = 3;
-    const [time, setTime] = useState(15);
     const hint: number = 2;
     const count: number = 5;
+    const [time, setTime] = useState(15);
+
+    const getListState = () => {
+        let states = new Array(stagePoint[stage].length).fill(false);
+        // console.log("#### getListState states: " + JSON.stringify(states) + " / introductions: " + JSON.stringify(introductions));
+        return states;
+    };
+    const [pointStates, setPointStates] = useState(getListState);
+
+    useEffect(() => {
+
+    }, []);
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -30,19 +68,69 @@ const SpotTheDifference = () => {
         return () => {
             clearInterval(timer);
         };
-
     }, [time]);
 
     const imageRef = useRef<HTMLImageElement | null>(null);
 
     const onClickImage = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-        // const rect = imageRef.current.getBoundingClientRect();
-        const testX = event.clientX;
-        const testY = event.clientY;
+        if (!imageRef.current) return;
 
+        console.log("#### test: " + event.currentTarget.naturalWidth);
+        const target = event.currentTarget;
+        const rect = event.currentTarget.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
 
-        console.log("#### testX: " + testX+ " / testY: "+ testY);
-      };
+        const width = imageRef.current.width;
+        const height = imageRef.current.height;
+
+        // const relativeX = x / width;
+        // const relativeY = y / height;
+
+        // const pointX = x / ;
+        // const pointY = y / height;
+        const clickX = Math.round((x / width) * 100) / 100;
+        const clickY = Math.round((y / height) * 100) / 100;
+
+        const naturalWidth = target.naturalWidth;
+        const naturalHeight = target.naturalHeight;
+
+        const points: point[] = stagePoint[0];
+
+        let successIdx: number = -1;
+
+        points.forEach(function (p, i) {
+            if (!pointStates[stage][i]) {
+                const pointX = Math.round((p.x / naturalWidth) * 100) / 100;
+                const pointY = Math.round((p.y / naturalHeight) * 100) / 100;
+                console.log("#### clickX: " + clickX + " / pointX: " + pointX + " / clickY: " + clickY + " / pointY: " + pointY + " / successIdx: " + successIdx);
+
+                if (Math.abs(clickX - pointX) <= 0.05 && Math.abs(clickY - pointY) <= 0.05) {
+                    // console.log("#### x: " + x + " / y: " + y + " / width: " + width + " / height: " + height + " / clickX: " + clickX + " / clickY: " + clickY);
+                    console.log("###");
+                    successIdx = i;
+                }
+            }
+        });
+
+        // for (const p of points) {
+        //     // console.log(p);
+        //     const pointX = Math.round((p.x / naturalWidth) * 100) / 100;
+        //     const pointY = Math.round((p.y / naturalHeight) * 100) / 100;
+        //     // console.log("### pointX: " + pointX + " / pointY: " + pointY);
+
+        //     console.log("#### clickX: " + clickX + " / pointX: " + pointX + " / clickY: " + clickY + " / pointY: " + pointY + " / successIdx: " + successIdx);
+        //     if (Math.abs(clickX - pointX) <= 0.05 && Math.abs(clickY - pointY) <= 0.05) {
+        //         // console.log("#### x: " + x + " / y: " + y + " / width: " + width + " / height: " + height + " / clickX: " + clickX + " / clickY: " + clickY);
+        //         successIdx 
+        //         break;
+        //     } else {
+
+        //     }
+        // }
+
+        // console.log("#### x: " + x + " / y: " + y + " / width: " + width + " / height: " + height + " / clickX: " + clickX + " / clickY: " + clickY);
+    };
 
     // text-3xl font-semibold ${time > 10 ? text-black : text-red-50}
     return (
@@ -65,10 +153,20 @@ const SpotTheDifference = () => {
                         <div className={"text-3xl font-semibold " + (time > 10 ? "text-black" : "text-red-700")}>{time}</div>
                     </div>
                 </div>
-                <div className="w-11/12 h-1/2 max-h-[55rem] flex justify-evenly border-4 border-[#1c1917] bg-white">
-                    <img src={process.env.PUBLIC_URL + '/img/SpotTheDifference/stage_1_1.png'} alt="" className="w-5/12 object-contain" onClick={onClickImage}/>
-                    <div className="h-full border-[#1c1917] border-r-4 border-dotted"></div>
-                    <img src={process.env.PUBLIC_URL + '/img/SpotTheDifference/stage_1_2.png'} alt="" className="w-5/12 object-contain" onClick={onClickImage}/>
+                {/* <div className="w-11/12 h-1/2 max-h-[55rem] flex justify-evenly border-4 border-[#1c1917] bg-white"> */}
+                {/* <div className="w-10/12 h-1/2 max-h-[55rem] flex justify-evenly border-4 border-[#1c1917] bg-white"> */}
+
+                <div className="w-10/12 h-1/2 max-h-[55rem] flex border-4 border-[#1c1917] bg-white box-border">
+                    <div className="relative w-full h-full">
+                        <div className="absolute w-1/2 h-full inset-x-0 bg-black">
+                        </div>
+                        <div className="absolute w-1/2 h-full inset-x-1/2 bg-green-100">
+                        </div>
+                        {/* <img src={stageImg[stage][0]} alt="" className="absolute w-1/2 inset-x-0" onClick={onClickImage} ref={imageRef} />
+                    <img src={stageImg[stage][0]} alt="" className="absolute w-1/2 inset-x-0" onClick={onClickImage} ref={imageRef} />
+                    <img src={stageImg[stage][1]} alt="" className="absolute w-1/2 inset-x-1/2" onClick={onClickImage} ref={imageRef} /> */}
+
+                    </div>
                 </div>
                 <div className="w-4/5 flex row gap-x-10 justify-between">
                     <div className="text-3xl font-semibold">Count: 2/{count}</div>
